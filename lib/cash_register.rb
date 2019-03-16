00497ce71f15f5883750721404b961a2dd1700ff
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-16 15:07:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(*discount)
+    @total = 0 
+    @discount = discount
+  end
+  
+  def add_item(item, price, *quantity)
+    @total += price 
+  end
+  
+end
